3e00b5585bd6d98aa8fbc9abd04076e7ee66d5f8
Russell Gordon <rgordon@lcs.on.ca>
2022-11-05 06:48:42 -0400

    Initial Commit

---
+++ HeaderExample/HeaderExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderExampleApp.swift
+//  HeaderExample
+//
+//  Created by Russell Gordon on 2022-11-05.
+//
+
+import SwiftUI
+
+@main
+struct HeaderExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
